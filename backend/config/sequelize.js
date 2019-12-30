ab89ebf1ef088aa0e636b42e58c52e0688be7f23
Jimin Yeon <gmini.y@gmail.com>
2019-12-31 02:00:27 +0900

    refactor: sequelize configuration dotenv

---
+++ backend/config/sequelize.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": process.env.MYSQL_PASSWORD,
+    "database": "courtFinder",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  }
+};
