53c9ad98ec08a75865a39e42e5da9dbfea69b11e
Jimin Yeon <gmini.y@gmail.com>
2020-01-08 00:12:16 +0900

    [feat] match, filter 기능 구현
    - match는 name, address에서만 like로 구현(성능평가 해보기)
    - filter는 city, district

--- backend/routes/courts/courts-ctrl.js
+++ backend/routes/courts/courts-ctrl.js
@@ -1,3 +1,24 @@
-exports.search = (req, res, next) => {
-  return res.send('hello this is search controller.');
-}
+const { Court, Sequelize: { Op } } = require('../../models');
+const createError = require('http-errors');
+
+exports.search = async (req, res, next) => {
+  const { page, size, query: { match, filter } } = req.body;
+
+  if (!page || !size) return next(createError(400, "page and size are required"));
+
+  const where = { [Op.or]: [
+    {
+      name: { [Op.like]: `%${match}%` }
+    },
+    {
+      address: { [Op.like]: `%${match}%` }
+    }
+  ]};
+  
+  if (filter.city) where.city = filter.city;
+  if (filter.district) where.district = filter.district;
+
+  const courts = await Court.findAll({ where })
+
+  return res.json(courts);
+};
