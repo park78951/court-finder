8ffbce054665d6478e63bb5d75b9a46ab4c1f263
Jimin Yeon <gmini.y@gmail.com>
2020-01-08 00:33:35 +0900

    [feat] pagination 구현
    - limit, offset 추가
    - attribute 에서 timestamp 관련은 제외

--- backend/routes/courts/courts-ctrl.js
+++ backend/routes/courts/courts-ctrl.js
@@ -4,21 +4,37 @@ const createError = require('http-errors');
 exports.search = async (req, res, next) => {
   const { page, size, query: { match, filter } } = req.body;
 
-  if (!page || !size) return next(createError(400, "page and size are required"));
-
-  const where = { [Op.or]: [
-    {
-      name: { [Op.like]: `%${match}%` }
-    },
-    {
-      address: { [Op.like]: `%${match}%` }
-    }
-  ]};
+  if (!page || !size) return next(
+    createError(400, "page and size should be defined minimum 1")
+  );
   
-  if (filter.city) where.city = filter.city;
-  if (filter.district) where.district = filter.district;
+  const config = {
+    offset: (page -1 ) * size,
+    limit: size,
+    where: { [Op.or]: [
+      {
+        name: { [Op.like]: `%${match}%` }
+      },
+      {
+        address: { [Op.like]: `%${match}%` }
+      }
+    ]},
+    attribute: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] }
+  };
 
-  const courts = await Court.findAll({ where })
+  if (filter.city) config.where.city = filter.city;
+  if (filter.district) config.where.district = filter.district;
+  
+  try {
+    const data = await Court.findAndCountAll(config);
 
-  return res.json(courts);
+    const sendingData = {
+      hasNextPage: Math.ceil(data.count/size) > page,
+      courts: data.rows
+    }
+
+    return res.json(sendingData);
+  } catch (error) {
+    next(error);
+  }
 };
