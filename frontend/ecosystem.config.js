7de790b3bda3d8680f025604591dfc587afc6bfd
gminiy <gmini.y@gmail.com>
2020-02-13 11:15:35 +0900

    feat: add pm2 ecosystem fil

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,12 @@
+odule.exports = {
+  apps : [{
+    name: 'frontend',
+    script: './server.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    autorestart: true,
+    watch: true,
+    watch_delay: 1000,
+    ignore_watch: ["README.md"]
+  }],
+};
