6a2e19d91f99e7c1db45fd10f34dad275eda6b40
gminiy <gmini.y@gmail.com>
2020-02-13 11:22:48 +0900

    fix: init script directory edit

--- init/init-env.sh
+++ init/init-env.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # package install
-npm install --prefix /home/ec2-user/backend
-npm install --prefix /home/ec2-user/frontend
+npm install --prefix /home/ec2-user/build/backend
+npm install --prefix /home/ec2-user/build/frontend
 
 # ssr server build & start
 sudo npm run build --prefix /home/ec2-user/build/frontend
